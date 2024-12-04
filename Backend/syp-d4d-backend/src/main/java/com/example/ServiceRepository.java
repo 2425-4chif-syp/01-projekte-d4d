a145a9c32e5489cff2533d3fcc06bd685ea51ae8
BirklbauerMichael <m.birklbauer@students.htl-leonding.ac.at>
2024-12-04 15:27:37 +0100

    update: renaming classes for better understanding

--- Backend/syp-d4d-backend/src/main/java/com/example/ServiceRepository.java
+++ Backend/syp-d4d-backend/src/main/java/com/example/ServiceRepository.java
@@ -23,6 +23,7 @@ public abstract class UserRepository {
             return offers;
         }
 
+
         for (User u : users) {
             if (u.getServiceOffer().equals(serviceOffer)) {
                 offers.add(u);
