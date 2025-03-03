850fdaf4df9133d0d461711b30a501e42cb2ed8d
Leon Zehetner <95644720+lzehetner@users.noreply.github.com>
2025-03-03 10:30:03 +0100

    feat: add user resource

---
+++ Backend/syp-d4d-backend/src/main/java/at/htl/d4d/endpoints/UserResource.java
@@ -0,0 +1,39 @@
+package at.htl.d4d.endpoints;
+
+import at.htl.d4d.control.UserRepository;
+import at.htl.d4d.entity.Service;
+import at.htl.d4d.entity.User;
+import jakarta.ws.rs.Produces;
+import jakarta.inject.Inject;
+import jakarta.json.JsonObject;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+
+public class UserResource {
+
+    @Inject
+    UserRepository userRepository;
+
+    public UserResource() {
+    }
+    
+    @POST
+    @Path("/createUserIfNotExists")
+    @Produces(MediaType.TEXT_PLAIN)
+    @Transactional
+        public Response createUser(JsonObject userJson) {
+        String userName = userJson.getString("name");
+
+        if (!userRepository.existsByName(userName)) {
+            User newUser = new User(userName);
+            userRepository.persist(newUser);
+            return Response.ok("User created successfully!").build();
+        }
+
+        return Response.ok("Successfully").build();
+    }
+}
