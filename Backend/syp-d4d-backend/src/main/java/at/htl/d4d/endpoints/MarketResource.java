45bd4ad47e392b3bbb2d94a1106c4d8df7e99af8
Leon Zehetner <95644720+lzehetner@users.noreply.github.com>
2025-03-03 10:29:28 +0100

    feat: add market resource

---
+++ Backend/syp-d4d-backend/src/main/java/at/htl/d4d/endpoints/MarketResource.java
@@ -0,0 +1,58 @@
+package at.htl.d4d.endpoints;
+
+import at.htl.d4d.control.MarketRepository;
+import at.htl.d4d.control.ServiceTypesRepository;
+import at.htl.d4d.control.UserRepository;
+import at.htl.d4d.entity.Market;
+import at.htl.d4d.entity.ServiceType;
+import at.htl.d4d.entity.User;
+import jakarta.inject.Inject;
+import jakarta.json.JsonObject;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path("d4d")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class MarketResource {
+
+    @Inject
+    UserRepository userRepository;
+    
+    @Inject 
+    MarketRepository marketRepository;
+
+    public MarketResource() {
+    }
+
+    @POST
+    @Path("/createMarket")
+    @Produces(MediaType.TEXT_PLAIN)
+    @Transactional
+    public Response createService(JsonObject userJson) {
+
+        String userName = userJson.getString("name");
+        String serviceOffer = userJson.getString("serviceOffer");
+        String serviceWanted = userJson.getString("serviceWanted");
+
+        User user = userRepository.findUserByName(userName);
+
+        ServiceType offerServiceType = ServiceTypesRepository.findServiceTypeByName(serviceOffer);
+        ServiceType wantedServiceType = ServiceTypesRepository.findServiceTypeByName(serviceWanted);
+
+        Market offerMarket = new Market(offerServiceType.id, user.id, 1);
+        Market wantedMarket = new Market(wantedServiceType.id, user.id, 0);
+
+        return Response.ok("Market created successfully").build();
+    }
+
+
+
+
+}
