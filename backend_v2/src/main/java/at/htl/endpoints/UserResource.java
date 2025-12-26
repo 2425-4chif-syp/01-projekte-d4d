94549da8817c82a0e7adb82752b1f789aa586249
jelicstefan <s.jelic@students.htl-leonding.ac.at>
2025-12-26 22:11:36 +0100

    fix: disable oidc login remove jwt injection

--- backend_v2/src/main/java/at/htl/endpoints/UserResource.java
+++ backend_v2/src/main/java/at/htl/endpoints/UserResource.java
@@ -4,19 +4,13 @@ import at.htl.entity.User;
 import at.htl.entity.Session;
 import at.htl.repository.UserRepository;
 import at.htl.repository.SessionRepository;
-import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
-import jakarta.ws.rs.core.Cookie;
-import jakarta.ws.rs.core.Context;
-import jakarta.ws.rs.core.HttpHeaders;
 import jakarta.ws.rs.core.NewCookie;
 import java.util.Map;
-import io.quarkus.security.Authenticated;
-import org.eclipse.microprofile.jwt.JsonWebToken;
 
 @Path("user")
 @Produces(MediaType.APPLICATION_JSON)
@@ -27,9 +21,6 @@ public class UserResource {
     
     @Inject
     SessionRepository sessionRepository;
-
-    @Inject
-    JsonWebToken jwt;
 
     @POST
     @Path("keycloak")
